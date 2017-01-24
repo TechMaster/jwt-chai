20adc5c17b1c3002c9fe3e0c5374e887a79776e6
minhcuong <cuong@techmaster.vn>
2017-01-24 12:28:06 +0700

    Add 401 test

--- test/02_login.js
+++ test/02_login.js
@@ -13,6 +13,15 @@ let token; //store token return from /login
 
 describe("Login to get JWT token", () => {
 
+  it("It should return 401 when get /secret without token", (done) => {
+    chai.request(server)
+      .get('/secret')
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
+  });
+
   it("Login user name and password to get back token ", (done) => {
     chai.request(server)
       .post('/login')
@@ -28,11 +37,11 @@ describe("Login to get JWT token", () => {
   });
 
 
-  it("Try to get secret data at /secret", (done) => {
+  it("It should return 200 when get /secret wit token", (done) => {
     if (sessionStorage.token) {
       chai.request(server)
         .get('/secret')
-        .set('content-type', 'application/x-www-form-urlencoded')
+        // .set('content-type', 'application/x-www-form-urlencoded')
         .set('Authorization', 'JWT '.concat(sessionStorage.token))
         .end((err, res) => {
           res.should.have.status(200);
